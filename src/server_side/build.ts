e3ca25bd8626cbf6990f69212fa7184f83be46f4
thatretrodev <thatretrodev@outlook.com>
2021-05-01 13:12:27 -0400

    Change some code in build.ts and add web server code to start_web_server.ts

--- src/server_side/build.ts
+++ src/server_side/build.ts
@@ -9,6 +9,12 @@
 	const { exec } = require('child_process');
 
 /*
+	Create some directories that are needed.
+	TODO: Use "mkdirp" since that seems to be what other people use for creating directories in nodejs.
+*/
+exec("mkdir bin/assets");
+
+/*
 	Compile the server-side code
 	TODO: Optimize this code so that I don't need to add a line for every TypeScript source code file!
 	*/
--- src/server_side/start_web_server.ts
+++ src/server_side/start_web_server.ts
@@ -1,2 +1,18 @@
-// TODO: Add some actual web server code here
-console.log("placeholder");
+/*
+	Filename: start_web_server.ts
+	This file (obviously) starts a local web server that is used for testing the website.
+*/
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.use(express.static('bin/assets'));
+
+app.get('/', (req, res) => {
+	res.send('<title>Test!</title><h1>Test!</h1>');
+});
+
+app.listen(port, () => {
+	console.log(`Testing server listening at http://localhost:${port}`);
+});
