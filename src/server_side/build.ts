c0b4b7d450fa47a77a51c04cbce0c5505775b215
thatretrodev <thatretrodev@outlook.com>
2021-05-01 13:56:00 -0400

    Add status info code to build.ts

--- src/server_side/build.ts
+++ src/server_side/build.ts
@@ -17,20 +17,24 @@ exec("mkdir bin/assets");
 /*
 	Compile the server-side code
 	TODO: Optimize this code so that I don't need to add a line for every TypeScript source code file!
-	*/
-	
+*/
+
+console.log("Compiling start_web_server.ts...");
 exec("npx tsc --outFile bin/server_side/start_web_server.js src/server_side/start_web_server.ts"); // start_web_server.ts
+console.log("Compiling webpack.config.ts...");
 exec("npx tsc --outFile bin/server_side/webpack.config.js src/server_side/webpack.config.ts"); // webpack.config.ts
 
 /*
 	Compile the client-side code
 	TODO: Optimize this code so that I don't need to add a line for every TypeScript source code file!
-	*/
+*/
 
-	exec("npx tsc --outFile bin/client_side/main.js src/client_side/main.ts"); // main.ts
+console.log("Compiling main.ts...");
+exec("npx tsc --outFile bin/client_side/main.js src/client_side/main.ts"); // main.ts
 
 /*
 	Bundle the client-side code using webpack
 */
 
+console.log("Bundling client-side code...");
 exec("npx webpack --config bin/server_side/webpack.config.js");
