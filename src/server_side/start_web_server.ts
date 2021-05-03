0068ae2d87eb313e00ba05b47624d25abf397b6c
thatretrodev <thatretrodev@outlook.com>
2021-05-02 23:49:38 -0400

    Add a simple blog page

--- src/server_side/start_web_server.ts
+++ src/server_side/start_web_server.ts
@@ -19,8 +19,13 @@ function getRandomInt(min, max) {
 
 
 app.get('/', (req, res) => {
-	res.render('index', {randomnumberthing: getRandomInt(0, 99999999)});
+	res.render('index');
+	//res.render('index', {randomnumberthing: getRandomInt(0, 99999999)});
 	//res.send('<head><link rel="stylesheet" href="css/bootstrap.min.css"><script src="js/main.js"></script></head><body><title>Test!</title><h1>Test!</h1></body>');
+});
+
+app.get('/blog', (req, res) => {
+	res.render('blog_index', {blog_posts: [["test", "02/05/2021", "Hello, World!", 0], ["test", "02/05/2021", "Hello, World!", 0]]});
 });
 
 app.listen(port, () => {
