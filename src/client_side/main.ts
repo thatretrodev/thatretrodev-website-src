8f155a4000256dfa1103ed6c9480e450269c49f2
thatretrodev <thatretrodev@outlook.com>
2021-05-03 16:11:05 -0400

    Add major code changes

--- src/client_side/main.ts
+++ src/client_side/main.ts
@@ -1,6 +1,8 @@
 var $ = require("jquery");
 import 'bootstrap';
 
+console.log("Thatretrodev's website code @ https://github.com/thatretrodev/thatretrodev-website-src is now running!");
+
 window.onload = () => {
 	var test_element = document.createElement('p');
 	test_element.innerText = "This element was created using TypeScript code with webpack. Hello, World!";
--- src/server_side/start_web_server.ts
+++ src/server_side/start_web_server.ts
@@ -3,12 +3,14 @@
 	This file (obviously) starts a local web server that is used for testing the website.
 */
 
-const express = require('express');
+const express = require("express");
+var path = require("path");
+var jsdom = require("jsdom");
+let ejs = require('ejs');
 const app = express();
 const port = 8080;
 
 app.set('view engine', 'ejs');
-
 app.use(express.static('bin/assets'));
 
 function getRandomInt(min, max) {
@@ -17,6 +19,68 @@ function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
 
+function create_preview_from_blog_post(blog_post_path) {
+	var html_data = "";
+	var post_text = "";
+
+	ejs.renderFile(blog_post_path, {}, function(err, str){
+		html_data = str;
+	});
+
+	const { window } = new jsdom.JSDOM(html_data);
+	var $ = require('jquery')(window);
+	post_text = $("#post_contents").text(); //window.document.getElementById("post_contents").innerText;
+	console.log("DEBUG STUFF: " + post_text);
+	if (new String(post_text).length > 64) {
+		post_text = post_text.substring(0, 100) + "...";
+	}
+	
+	return post_text;
+}
+
+function get_title_from_blog_post(blog_post_path) {
+	var html_data = "";
+	var post_title = "";
+
+	ejs.renderFile(blog_post_path, {}, function(err, str){
+		html_data = str;
+	});
+
+	const { window } = new jsdom.JSDOM(html_data);
+	post_title = window.document.querySelector('meta[name="blog_post_title"]').content;
+
+	return post_title;
+}
+
+function get_date_from_blog_post(blog_post_path) {
+	var html_data = "";
+	var post_date = "";
+
+	ejs.renderFile(blog_post_path, {}, function(err, str){
+		html_data = str;
+	});
+
+	const { window } = new jsdom.JSDOM(html_data);
+	post_date = window.document.querySelector('meta[name="blog_post_date"]').content;
+
+	return post_date;
+}
+
+//var blog_posts = [];
+var blog_posts:string[][] = [];
+
+function create_blog_post_pages(expressapp) {
+	var finder = require('findit')(__dirname + "/../../views/blog_posts");
+	finder.on('file', function (file) {
+		//console.log('Found: ' + path.basename(file));
+		console.log("Adding blog post page: " + '/blog/' + path.basename(file).replace(".ejs", ""));
+		blog_posts.push([get_title_from_blog_post(file), get_date_from_blog_post(file), create_preview_from_blog_post(file), path.basename(file).replace(".ejs", "")]);
+		expressapp.get('/blog/' + path.basename(file).replace(".ejs", ""), (req, res) => {res.render("blog_post_viewer", {blog_post_template_file: 'blog_posts/' + path.basename(file).replace(".ejs", "")});}); // TODO: This WILL break if you put a blog post template inside a directory that's inside blog_posts.
+	});
+}
+
+create_blog_post_pages(app);
+setTimeout(function() {console.log(blog_posts);}, 100);
 
 app.get('/', (req, res) => {
 	res.render('index');
@@ -25,7 +89,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/blog', (req, res) => {
-	res.render('blog_index', {blog_posts: [["test", "02/05/2021", "Hello, World!", 0], ["test", "02/05/2021", "Hello, World!", 0]]});
+	res.render('blog_index', {blog_posts: blog_posts});
 });
 
 app.listen(port, () => {
